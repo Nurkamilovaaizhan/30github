a25dfc72ba8899a586fe806c8bc20e8d1ca7e098
aizhan <nurkamilovaaizhan@gmail.com>
2002-02-13 21:20:51 +0600

    day7

--- src/string_methods.java
+++ src/string_methods.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class string_methods {
     public static void main(String[] args) {
 
-        String name = "Aizhan";
+//        String name = "Aizhan";
 
 //        int length = name.length();
 //        int letter = name.charAt(3);
@@ -15,6 +17,36 @@ public class string_methods {
 
 //        name = name.isEmpty();
 //        and other
+
+//        day7
+//        substring() - is a method used to extract a portion of a string
+//          .substring(start, end);
+
+//        String username = email.substring(0, 6);
+
+//        email = "aizhan777@gmail.com";
+//        username = email.substring(0, email.indexOf("@"));
+//        domain = email.substring(email.indexOf("@") + 1);
+
+
+//        String domain = email.substring(10);
+
+        Scanner sc = new Scanner(System.in);
+        String email;
+        String username;
+        String domain;
+
+        email = sc.nextLine();
+        username = email.substring(0, email.indexOf("@"));
+        domain = email.substring(email.indexOf("@") + 1);
+
+        System.out.println("Enter your email: ");
+        email = sc.nextLine();
+
+        System.out.println(email);
+
+        sc.close();
+
 
 
     }
