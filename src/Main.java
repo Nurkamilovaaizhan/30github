b1622d5cf31434a4cf8626316db09fa79252b0f9
aizhan <nurkamilovaaizhan@gmail.com>
2025-02-08 20:52:14 +0600

    day2

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        output
-//        System.out.println("Hello World");
-
-//        input
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter your name: ");
-//        in one line "printIn without ln"
-        String name = scanner.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-
-        System.out.print("Enter your gpa: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-
-        System.out.println("Hello " + name);
-        System.out.println("Your age is " + age);
-        System.out.println("Your gpa is " + gpa);
-        System.out.println("Is student? " + isStudent);
-
-        scanner.close();
-
+        /*day 2*/
+//       if statements
+        int age = -1;
+        if (age >= 18){
+            System.out.println("You are an adult");
+        } else if (age < 0){
+            System.out.println("U have`nt been born yet");
+        }
+        else{
+            System.out.println("molokosos");
+        }
     }
 }
+/*day1*/
+//        output
+//        System.out.println("Hello World");
+//        input
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter your name: ");
+//        in one line "printIn without ln"
+//        String name = scanner.nextLine();
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.print("Enter your gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("Are you a student? (true/false): ");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("Your age is " + age);
+//        System.out.println("Your gpa is " + gpa);
+//        System.out.println("Is student? " + isStudent);
+//        scanner.close();
+
+
+
+
