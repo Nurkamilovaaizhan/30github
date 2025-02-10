2a926c9aa93674e51496b1be95180c5dbba83875
aizhan <nurkamilovaaizhan@gmail.com>
2025-02-10 11:49:26 +0600

    day4

---
+++ src/day4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class day4 {
+    public static void main(String[] args) {
+
+//        printf() - is a method used to format output
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    }
+}
