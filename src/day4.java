d6523071d1ff2023fc3da025995cca816b0e6116
aizhan <nurkamilovaaizhan@gmail.com>
2025-02-11 01:23:42 +0600

    day5

--- src/day4.java
+++ src/day4.java
@@ -4,8 +4,19 @@ public class day4 {
     public static void main(String[] args) {
 
 //        printf() - is a method used to format output
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
+//        %[flags][width][.precision][specifier-character]
+        String name = "Stefan";
+        char firstLetter = 'S';
+        int age = 169;
+        double height = 182.0;
+        boolean isVampire = true;
+
+        System.out.printf("My husband's name is: %s\n", name);
+        System.out.printf("The name starts with: %c\n", firstLetter);
+        System.out.printf("My husband's age is: %d\n", age);
+        System.out.printf("He is %f inches tall \n", height);
+        System.out.printf("Vampire: %b\n", isVampire);
+
+        System.out.printf("%s is %d years old \n", name, age);
     }
 }
