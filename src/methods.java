9017d14a7106c786a7c12fac2c2eed398ad775da
aizhan <nurkamilovaaizhan@gmail.com>
2025-02-18 11:53:48 +0600

    day12

--- src/methods.java
+++ src/methods.java
@@ -3,14 +3,26 @@ public class methods {
 
 //        method - a block of reusable code that is executed when called ()
 
-        String name = "Aizhan";
-        int age =25;
-        happyBirthday(name, age);
+//        String name = "Aizhan";
+//        int age = 25;
+//        happyBirthday(name, age);
+
+        System.out.println(add(1, 2));
     }
-    static void happyBirthday(String name, int age) {
-        System.out.println("Happy Birthday to you");
-        System.out.printf("Happy Birthday dear %s!", name);
-        System.out.println("Happy Birthday to you");
-        System.out.println("Happy Birthday to you\n");
+//    static void happyBirthday(String name, int age) {
+//        System.out.println("Happy Birthday to you");
+//        System.out.printf("Happy Birthday dear %s!\n", name);
+//        System.out.printf("You are %d years old \n", age);
+//        System.out.println("Happy Birthday to you\n");
+//    }
+
+//    overloaded methods - that share the same name, but different paremeters
+//    signature = name + parameters
+
+
+
+    static double add(double a, double b) {
+        return a + b;
     }
+
 }
