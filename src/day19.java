5c6e6f055ac0fb0556ff109d82a6083358bd8728
aizhan <nurkamilovaaizhan@gmail.com>
2025-02-26 22:21:51 +0600

    20

--- src/day19.java
+++ src/day19.java
@@ -4,6 +4,8 @@ public class day19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        .jdgh.zsglhzsghjzdhfglzhjzhfl
+                xjlzhsgskdlghslJG
 
     }
 }
